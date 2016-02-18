e226d936bc47f36b369c74d6b981e36a16cec4e9
Bill Rocha <Bill Rocha>
2016-02-18 05:25:10 -0200

    initial

---
+++ src/htlm.config.php
@@ -0,0 +1,8 @@
+<?php 
+
+return ['html_path' => '',
+		'html_cache' => '/cache/',
+		'js_path' => '',
+		'js_url' => 'js/',
+		'css_path' => '',
+		'css_url' => 'css/'];
---
+++ src/neostag.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * NeosTag 
+ * @copyright   Bill Rocha - http://plus.google.com/+BillRocha
+ * @license     MIT & GLP2
+ * @author      Bill Rocha - prbr@ymail.com
+ * @version     0.0.1
+ * @package     Frame\\Doc
+ * @access      public
+ * @since       0.0.4
+ *
+ * ©NeosTag é marca registrada da NeosOrg e todos os direitos são reservados.
+ */
+
+namespace Frame\Doc;
+
+class Neostag{
+
+
+
+
+}
